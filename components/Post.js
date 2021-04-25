3ba5d43bda14a53882d1c24e0890d85d70d83208
TomoyukiMatsuda <tomoyuki.matsuda35@gmail.com>
2021-04-25 17:38:07 +0900

    posts詳細ページ作成

--- components/Post.js
+++ components/Post.js
@@ -3,9 +3,11 @@ export default function Post({ post }) {
     <div>
       <span>{post.id}</span>
       {"："}
-      <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
-        {post.title}
-      </span>
+      <Link href={`/posts/${post.id}`}>
+        <span className="cursor-pointer text-white border-b border-gray-500 hover:bg-gray-600">
+          {post.title}
+        </span>
+      </Link>
     </div>
   )
 }
